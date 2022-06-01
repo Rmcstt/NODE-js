65a75c5351865c3b46642e42fe7f6874661c4d10
rmcstt <anisio.nanato@hotmail.com>
2022-06-01 12:58:30 -0300

    export

---
+++ moduloA.js
@@ -0,0 +1,4 @@
+this.ola = 'fala pessoal'
+exports.bemvindo = 'bem vindo ao cod3r'
+module.exports.atelogo = 'ate o proximo exemplo'
+// maneiras de exportar um objeto pra fora do arquivo (modulo)
---
+++ moduloCliente.js
@@ -0,0 +1,25 @@
+const moduloA = require('./moduloA')
+const modulob = require('./modulob')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemvindo)
+console.log(moduloA.atelogo)
+console.log(moduloA)
+
+console.log(modulob.bomDia)
+console.log(modulob.boaNoite())
+console.log(modulob)
+
+/*
+fala pessoal
+bem vindo ao cod3r
+ate o proximo exemplo
+{
+  ola: 'fala pessoal',
+  bemvindo: 'bem vindo ao cod3r',
+  atelogo: 'ate o proximo exemplo'
+}
+bom dia
+boa noite
+{ bomDia: 'bom dia', boaNoite: [Function: boaNoite] } 
+*/
