dd4974ce138ce671b212b2826842c70839deca5f
rmcstt <anisio.nanato@hotmail.com>
2022-06-02 13:58:12 -0300

    vscode

--- pasta A/pasta B/acessarModulo.js
+++ pasta A/pasta B/acessarModulo.js
@@ -1,2 +1,2 @@
 const moduloA = require('../../moduloA') //da tambem para colocar o caminho absoluto "/Users/renatomota/Desktop/NODE/NODE-js/moduloA.js"
-console.log(moduloA.ola)
+console.log(moduloA.ola) ///fala pessoal
