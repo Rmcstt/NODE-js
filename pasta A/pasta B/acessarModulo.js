7695074fab47d2198448e7d79a7621c93a2fb530
rmcstt <anisio.nanato@hotmail.com>
2022-06-02 13:56:37 -0300

    vscode

---
+++ pasta A/pasta B/acessarModulo.js
@@ -0,0 +1,2 @@
+const moduloA = require('../../moduloA') //da tambem para colocar o caminho absoluto "/Users/renatomota/Desktop/NODE/NODE-js/moduloA.js"
+console.log(moduloA.ola)
---
+++ usandoModulosTerceiros.js
@@ -0,0 +1,5 @@
+const _ = require('lodash')
+setInterval(() => console.log(_.random(1, 10)), 5000)
+//npm i lodash instala uma biblioteca de terceiro
+
+// "sudo npm i -g nodemon" instala de forma global uma biblioteca que vai ficar rodando diretamente no ternminal
