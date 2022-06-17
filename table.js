d4aba641f88cf6c9e289e620007b58b8e19c462c
rmcstt <anisio.nanato@hotmail.com>
2022-06-16 23:08:32 -0300

    table

---
+++ table.js
@@ -0,0 +1,25 @@
+frutas = [
+  {
+    nome: 'banana',
+    quantidade: 22
+  },
+  {
+    nome: 'melancia',
+    quantidade: 33
+  },
+  {
+    nome: 'abacate',
+    quantidade: 32
+  }
+]
+
+console.table(frutas)
+/*
+┌─────────┬────────────┬────────────┐
+│ (index) │    nome    │ quantidade │
+├─────────┼────────────┼────────────┤
+│    0    │  'banana'  │     22     │
+│    1    │ 'melancia' │     33     │
+│    2    │ 'abacate'  │     32     │
+└─────────┴────────────┴────────────┘
+*/
